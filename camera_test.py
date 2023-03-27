8fdf44fb8265b3c0e82d24eac684cdc7bedb668e
Isabelle Bryenton <isabelle.bryenton@gmail.com>
2023-03-27 12:58:39 -0400

    circle detection

--- camera_test.py
+++ camera_test.py
@@ -21,15 +21,17 @@ def main():
     # load the furst image that contains an object that is KNOWN TO BE 2 feet
     # from our camera, then find the paper marker in the image, and initialize
     # the focal length
-    image = cv2.imread("images/2ft.png")
+    image = cv2.imread("images/2ft2.png")
     marker = find_marker(image)
     focalLength = (marker[1][0] * KNOWN_DISTANCE) / KNOWN_WIDTH
+    print(marker)
     # loop over the images
     for imagePath in sorted(paths.list_images("images")):
         # load the image, find the marker in the image, then compute the
         # distance to the marker from the camera
         image = cv2.imread(imagePath)
         marker = find_marker(image)
+        circles = find_circle(image)
         inches = distance_to_camera(KNOWN_WIDTH, focalLength, marker[1][0])
         # draw a bounding box around the image and display it
         box = cv2.cv.BoxPoints(marker) if imutils.is_cv2() else cv2.boxPoints(marker)
@@ -45,8 +47,11 @@ def main():
 def find_marker(image):
 	# convert the image to grayscale, blur it, and detect edges
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
-    edged = cv2.Canny(gray, 35, 125)
+    cv2.imshow("gray", gray)
+    #gray = cv2.GaussianBlur(gray, (5, 5), 0)
+    gray=cv2.medianBlur(gray,5)
+    edged = cv2.Canny(gray, 25, 80)
+    cv2.imshow("edged", edged)
     # find the contours in the edged image and keep the largest one;
     # we'll assume that this is our piece of paper in the image
     cnts = cv2.findContours(edged.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
@@ -54,6 +59,26 @@ def find_marker(image):
     c = max(cnts, key = cv2.contourArea)
     # compute the bounding box of the of the paper region and return it
     return cv2.minAreaRect(c)
+
+def find_circle(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray=cv2.medianBlur(gray,5)
+    rows = gray.shape[0]
+    circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, rows / 8,
+                               param1=100, param2=30,
+                               minRadius=1, maxRadius=80)
+    if circles is not None:
+        circles = np.uint16(np.around(circles))
+        for i in circles[0, :]:
+            center = (i[0], i[1])
+            # circle center
+            cv2.circle(image, center, 1, (0, 100, 100), 3)
+            # circle outline
+            radius = i[2]
+            cv2.circle(image, center, radius, (255, 0, 255), 3)
+
+    cv2.imshow("detected circles", image)
+    cv2.waitKey(0)
 
 
 def distance_to_camera(knownWidth, focalLength, perWidth):
