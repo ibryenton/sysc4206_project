65dafdc83a37d4d2931612047328d1f2b143ecef
Isabelle Bryenton <isabelle.bryenton@gmail.com>
2023-03-27 12:04:53 -0400

    script add

---
+++ camera_test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+Script to experiment with OpenCV object detection using images from iphone camera
+"""
+
+def main():
+    """
+    <Main Description>
+    """
+
+if __name__ == '__main__':
+    main()
+
+
