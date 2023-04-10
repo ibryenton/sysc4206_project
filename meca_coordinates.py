84d15e3bb87c46bb30a9786badcde4c8b2a44852
Isabelle Bryenton <isabelle.bryenton@gmail.com>
2023-04-09 20:09:47 -0400

    clean up meca transformation function

--- meca_coordinates.py
+++ meca_coordinates.py
@@ -15,13 +15,13 @@ class Meca():
         self.robot = mdr.Robot()
         self.callbacks = mdr.RobotCallbacks()
         self.callbacks.on_connected = self.print_connected()
-        self.robot.RegisterCallbacks(callbacks=self.callbacks, run_callbacks_in_separate_thread=True)
+        self.robot.RegisterCallbacks(callbacks=self.callbacks, run_callbacks_in_separate_thread=False)
         self.robot.Connect(address='192.168.0.100', enable_synchronous_mode=True)
         self.reset_error()
+        self.robot.ResumeMotion()
         self.robot.ActivateAndHome()
         self.robot.SetJointVel(10)
         self.robot.MoveJoints(0, 0, 0, 0, 0, 0)
-        self.robot.MoveJoints(0, -60, 60, 0, 0, 0)
 
     # The returned robot position will be (0, -60, 60, 0, 0, 0), because this line will only be executed once MoveJoints(0, -60, 60, 0, 0, 0) has completed.
         print(self.robot.GetJoints())
@@ -34,22 +34,32 @@ class Meca():
         robot.Disconnect()
 
     def print_connected(self):
-        print(f'[bold aquamarine1] connected')
+        return print(f'[bold aquamarine1] connected')
 
 
     def meca_coordinates(self, px, py, pz):
+        """
+        Function which takes coordinates of point in Camera Reference Frame
+        and returns the coordinates in Robot's Base Reference Frame
+
+        Args:
+            px (int): x coordinate of point in CRF
+            py (int): y coordinate of point in CRF
+            pz (int): z coordinate of point in CRF
+
+        Returns:
+            meca_coord (array): [x, y, z, 1] coordinates of point in BRF
+        """
         # Pc is the vector which defines the BRF to the CRF
-        Pc = [0, 320, 450, 1]
-        P = [px, py, pz, 1]
+        Pc = [300, -30, 420]
+        P = [px, -py, pz-25, 1]
 
         H = [[0, 1, 0, Pc[0]],
-             [1, 0, 0, Pc[1]],
+             [-1, 0, 0, Pc[1]],
              [0, 0, -1, Pc[2]],
              [0, 0, 0, 1]]
 
-        print(H)
         meca_coord = np.matmul(H,P)
-        print(meca_coord)
         return meca_coord
 
     def get_joints(self):
